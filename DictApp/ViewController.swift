3e7565b9b8721cae59b0f931a4403108c09f92f7
Tomonori Ueno <t.uepon@gmail.com>
2021-04-17 13:33:00 +0700

    Initial Commit

---
+++ DictApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DictApp
+//
+//  Created by SEZAX development on 17/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
